08e147e06343409b94b4d2e9e09357ea15de75b0
linak98 <59049500+linak98@users.noreply.github.com>
2020-01-15 03:32:24 +0200

    Update Τραμ.php

--- Τραμ.php
+++ Τραμ.php
@@ -1,3 +1,4 @@
+<?php include ('Εύρεση-Βέλτιστης.php'); ?>
 <!DOCTYPE html>
 <html lang="el">
 
@@ -134,6 +135,29 @@
                                     <a class="nav-link" href="#">Επικοινωνία</a>
                                 </li>
                             </ul>
+                            <?php
+                            $db=mysqli_connect('localhost','root','','test');
+                            $query42="SELECT `email` FROM `users` WHERE flag='1'";
+                            $result=mysqli_query($db,$query42);
+                            if(mysqli_num_rows($result)==1){
+                                
+                            ?>
+                            <div class="dorne-search-btn">
+                                <a id="search-btn" href="#"><i class="fa fa-search" aria-hidden="true"></i>Αναζήτηση</a>
+                            </div>
+                            <div class="content">
+                                       <style>
+                                       p{
+                                           margin: 0;
+                                       }
+                                       </style>
+                                      
+                                            <p><a style="color:white;">Καλωσήρθες</p> 
+                                            <p><strong><?php echo $_SESSION['email']; ?></a></strong></p>
+                                            <p><a href="Βέλτιστη-Διαδρομή.php?logout='0' " style="color:red;">Αποσύνδεση</a></p>
+                                            <p><a href="edit.php" name="edit" style="color:navy;" >Επεξεργασία Προφίλ</a></p>
+                            </div>
+                            <?php } else { ?>
                             <!-- Search btn -->
                             <?php 
                             if(isset($_POST['login'])){ ?>
@@ -150,7 +174,7 @@
                                         <?php if(isset($_SESSION["email"])): ?>
                                             <p><a style="color:white;">Καλωσήρθες</p> 
                                             <p><strong><?php echo $_SESSION['email']; ?></a></strong></p>
-                                            <p><a href="Βέλτιστη-Διαδρομή.php" style="color:red;">Αποσύνδεση</a></p>
+                                            <p><a href="Βέλτιστη-Διαδρομή.php?logout='0' " style="color:red;">Αποσύνδεση</a></p>
                                             <p><a href="edit.php" name="edit" style="color:navy;" >Επεξεργασία Προφίλ</a></p>
                                         <?php endif ?>
                     </div>
@@ -331,9 +355,6 @@ myInput.onkeyup = function() {
 
                         </div>
                     </nav>
-                    <?php 
-                            } 
-                            ?>
 					<nav class="h-30 navbar navbar-expand-lg">
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#dorneNav" aria-controls="dorneNav" aria-expanded="false" aria-label="Toggle navigation"><span class="fa fa-bars"></span></button>
                         <!-- Nav -->
@@ -345,6 +366,12 @@ myInput.onkeyup = function() {
 									<a class="nav-link" href="#"> > </a>
 									<a class="nav-link" href="Τραμ.php"><span style="color:rgba(0, 85, 132,1); font-weight: bold; font-size:18px;">ΤΡΑΜ</span></a>
                     </nav>
+                    <?php 
+                            } 
+                            ?>
+                            <?php 
+                            } 
+                            ?>
                 </div>
             </div>
         </div>
