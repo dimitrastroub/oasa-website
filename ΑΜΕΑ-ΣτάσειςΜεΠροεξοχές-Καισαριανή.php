a5f9ed9e32aad61a9fbc23922c1a99a10585f6f7
Chrysa24 <chrysavir@gmail.com>
2020-01-15 18:24:47 +0200

    Update ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Καισαριανή.php

--- ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Καισαριανή.php
+++ ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Καισαριανή.php
@@ -139,7 +139,7 @@
                                 </li>
                             </ul>
                             <?php
-                            $db=mysqli_connect('localhost','root','','test');
+                            $db=mysqli_connect('localhost','root','','sdi1600021');
                             $query42="SELECT `email` FROM `users` WHERE flag='1'";
                             $result=mysqli_query($db,$query42);
                             if(mysqli_num_rows($result)==1){
@@ -160,6 +160,8 @@
                                             <p><a href="Βέλτιστη-Διαδρομή.php?logout='0' " style="color:red;">Αποσύνδεση</a></p>
                                             <p><a href="edit.php" name="edit" style="color:navy;" >Επεξεργασία Προφίλ</a></p>
                             </div>
+						</div>
+					 </nav>
                             <?php } else { ?>
                             <!-- Search btn -->
                             <?php 
@@ -189,7 +191,7 @@
                             </div>
                             <!-- Signin btn -->
                             <div class="dorne-signin-btn">
-                                <a onclick="document.getElementById('id01').style.display='block'" style="font-style:normal; color:white; font-weight:600;">Είσοδος/Εγγραφή</a>
+                                <a onclick="document.getElementById('id01').style.display='block'" style="font-style:normal; color:white; font-weight:600; cursor: default;">Είσοδος/Εγγραφή</a>
                             </div> 
 						
                         <!--login-->
@@ -200,7 +202,7 @@
 									  <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
 									
 									</div>
-                                    <i style="font-weight:400; font-style:normal; font-size:20px; padding-left:18px;">Δεν είσαι μέλος;<a onclick="document.getElementById('id02').style.display='block'"><i style="color:lightblue; font-style:normal; font-size:20px;"> Εγγραφή</i></a></i>
+                                    <i style="font-weight:400; font-style:normal; font-size:20px; padding-left:18px;">Δεν είσαι μέλος;<a onclick="document.getElementById('id02').style.display='block'"><i style="color:lightblue; font-style:normal; font-size:20px; cursor: default;"> Εγγραφή</i></a></i>
 									<div class="container">
                                       
 									  <label for="uname" style="font-weight:400; font-size:20px;"><b>E-mail</b></label>
@@ -358,7 +360,11 @@ myInput.onkeyup = function() {
 
                         </div>
                     </nav>
-                    
+                     <?php 
+                            } 
+                            ?>
+                            <?php } ?>
+							
 					<nav class="h-30 navbar navbar-expand-lg">
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#dorneNav" aria-controls="dorneNav" aria-expanded="false" aria-label="Toggle navigation"><span class="fa fa-bars"></span></button>
                         <!-- Nav -->
@@ -373,10 +379,7 @@ myInput.onkeyup = function() {
 									<a class="nav-link" href="#"> > </a>
 									<a class="nav-link" href="ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Παπάγος.php"><span style="color:rgba(0, 85, 132,1); font-weight: bold; font-size:18px; ">Δήμος Καισαριανής</span></a> 
                     </nav>
-                    <?php 
-                            } 
-                            ?>
-                            <?php } ?>
+                   
                 </div>
             </div>
         </div>
@@ -400,7 +403,7 @@ myInput.onkeyup = function() {
                             <div class="tab-pane fade show active" id="nav-places" role="tabpanel" aria-labelledby="nav-places-tab">
                                 <h2><i class="fa fa-map-marker" aria-hidden="true"> </i> <i style =" font-style:normal;" >Στάση &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </i> <i  style =" font-style:normal;" >Οδός &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </i> <i  style =" font-style:normal;" >Δήμος &nbsp &nbsp &nbsp &nbsp &nbsp </i> <br><br>
 								<?php
-								$db=mysqli_connect('localhost','root','','test');
+								$db=mysqli_connect('localhost','root','','sdi1600021');
 								$sql= "SELECT * FROM kaisariani";
 								$result = $db->query($sql);
 								while($row = $result->fetch_assoc()) {?>
@@ -512,8 +515,7 @@ myInput.onkeyup = function() {
   });
 }
 
-var countries = ["Μετρό-Ωράριο","Τραμ", "ΜΜΜ","ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Καισαριανή","Μετρό-Στάσεις","Αγορά-Επαναφόρτιση", "Τιμές-Εισιτηρίων","ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Παπάγος","ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Ηλιούπολη","Βέλτιστη-Διαδρομή"];
-
+var countries = ["Μετρό-Ωράριο","Τραμ", "ΜΜΜ","ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Καισαριανή","Μετρό-Στάσεις","Αγορά-Επαναφόρτιση", "Τιμές-Εισιτηρίων","ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Παπάγος","ΑΜΕΑ-ΣτάσειςΜεΠροεξοχές-Ηλιούπολη", "Βέλτιστη-Διαδρομή","Δικαιολογητικά-Έκδοσης-Εισιτηρίων","Σημεία-Έκδοσης-Επαναφόρτισης"];
 
 autocomplete(document.getElementById("myInput"), countries);
 
